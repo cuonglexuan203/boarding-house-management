f292e2821560c6bd1b0a26a3f60b9f40e04e2584
cuonglexuan203 <cuonglexuan203@gmail.com>
2024-05-01 21:56:38 +0700

    upload client codebase

--- client/boarding-house-management-system/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-import { useAppSelector } from '@/libs/hooks';
-import { Button } from '@nextui-org/react';
-
-export default function Home() {
-  const value = useAppSelector((s) => s.status.value);
-  return <main>{value && <Button>Hello</Button>}</main>;
-}
