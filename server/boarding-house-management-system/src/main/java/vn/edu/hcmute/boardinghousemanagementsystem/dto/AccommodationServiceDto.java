4d6b3dd0862e187cb283aedf654291d2f07e0fa1
= <quocthong051@gmail.com>
2024-05-02 16:23:44 +0700

    refactor: modify AccommodationServiceDto from class to record

--- server/boarding-house-management-system/src/main/java/vn/edu/hcmute/boardinghousemanagementsystem/dto/AccommodationServiceDto.java
+++ server/boarding-house-management-system/src/main/java/vn/edu/hcmute/boardinghousemanagementsystem/dto/AccommodationServiceDto.java
@@ -1,23 +1,15 @@
 package vn.edu.hcmute.boardinghousemanagementsystem.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import vn.edu.hcmute.boardinghousemanagementsystem.entity.AccommodationService;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class AccommodationServiceDto {
-    private Long id;
-    private String name;
-    private float price;
-    private String unit;
-
+public record AccommodationServiceDto(
+        Long id,
+        String name,
+        float price,
+        String unit
+) {
     public AccommodationServiceDto(AccommodationService accommodationService) {
-        this.id = accommodationService.getId();
-        this.name = accommodationService.getName();
-        this.price = accommodationService.getPrice();
-        this.unit = accommodationService.getUnit();
+        this(accommodationService.getId(), accommodationService.getName(),
+                accommodationService.getPrice(), accommodationService.getUnit());
     }
 }
